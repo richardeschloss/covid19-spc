0d2c621196760d94e8788e69e7f4461d34a29546
Richard Schloss <richard.e.schloss@gmail.com>
2020-05-02 16:55:21 -0700

    Started making some functions more reusable

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="container">
+  <div class="container index-container">
     <div>
       <h1 class="title">
-        Covid19-spc
+        Covid19-spc: How's YOUR Region Doing?
+        <span v-show="$fetchState.pending">
+          (Fetching data...)
+        </span>
       </h1>
-      <state-select :states="states" @stateSelected="summarize" />
+      <p v-if="$fetchState.timestamp">
+        Data Fetched: {{ new Date($fetchState.timestamp).toLocaleString() }}
+      </p>
+      <combo-select
+        v-model="selectedState"
+        :items="casesInfo.states"
+        @itemSelected="stateSelected"
+        placeholder="Select State"
+        class="region-select"
+      />
       <div class="row">
         <trend-chart
-          :dates="casesDates"
+          :dates="casesInfo.dates"
           :trendData="totalCases"
           :selectedState="selectedState"
           :title="casesTitle"
           class="col-md-6"
         />
         <trend-chart
-          :dates="casesDates"
+          :dates="casesInfo.dates"
           :trendData="totalDeaths"
           :selectedState="selectedState"
           :title="deathsTitle"
@@ -28,38 +40,53 @@
 
 <script>
 import axios from 'axios'
-import StateSelect from '@/components/StateSelect'
+import ComboSelect from '@/components/ComboSelect'
 import TrendChart from '@/components/TrendChart'
 
 export default {
   components: {
     TrendChart,
-    StateSelect
+    ComboSelect
   },
   data() {
     return {
-      isOpen: false,
       selectedState: '',
-      casesHdr: [],
-      cases: [],
+      casesInfo: {},
       statesIdx: 0,
       states: [],
-      casesIdx: 0,
-      casesDates: [],
-      casesTitle: 'Cases (Daily Increase) -- [select state]',
       deathsTitle: 'Deaths (Daily Increase) -- [select state]',
       deathSubTitle: 'Deaths / Population:',
       totalCases: [],
-      totalDeaths: [],
-      showChart: false
+      totalDeaths: []
+    }
+  },
+  computed: {
+    casesTitle() {
+      return 'Cases (Daily Increase) -- ' + this.selectedState !== ''
+        ? this.selectedState
+        : '[select state]'
     }
   },
   fetch() {
-    this.getCases()
-    this.getDeaths()
+    const p = [this.getCases(), this.getDeaths()]
+    console.time('fetch')
+    Promise.all(p).then(([rawCases, rawDeaths]) => {
+      console.timeEnd('fetch')
+      this.parseCases(rawCases)
+    })
   },
   fetchOnServer: false,
   methods: {
+    calcTotals(matrix, colOffset) {
+      return matrix
+        .map((r) => r.slice(colOffset))
+        .reduce((cum, cols) => {
+          if (cum.length === 0) {
+            cum = Array(cols.length).fill(0)
+          }
+          return cols.map((col, idx) => (cum[idx] += parseInt(col)))
+        }, [])
+    },
     parseCsv(data) {
       return data
         .trim()
@@ -68,31 +95,48 @@ export default {
         .map((r) => r.split(','))
     },
     async getCases() {
-      console.time('casesFetched')
       const { casesUrl } = process.env
       const { data } = await axios.get(casesUrl)
-      console.timeEnd('casesFetched')
       const raw = this.parseCsv(data)
-      this.casesHdr = raw[0]
-      this.cases = raw.slice(1)
-      this.extractCasesDates()
-      this.extractStates()
+      return { hdr: raw[0], data: raw.slice(1) }
+    },
+    parseCases({ hdr, data }) {
+      const { datesIdx, dates } = this.extractDates(hdr, 'Combined_Key')
+      const { statesIdx, states } = this.extractStates(hdr, data)
+      Object.assign(this.casesInfo, {
+        datesIdx,
+        dates,
+        statesIdx,
+        states,
+        cases: data
+      })
+      this.selectedState = localStorage.getItem('selectedState') || ''
+      if (this.selectedState !== '') {
+        this.summarizeByState()
+      }
     },
     async getDeaths() {
-      console.time('deathsFetched')
       const { deathsUrl } = process.env
       const { data } = await axios.get(deathsUrl)
-      console.timeEnd('deathsFetched')
-      const raw = this.parseCsv(data)
-      this.deathsHdr = raw[0]
-      this.deaths = raw.slice(1)
-      this.extractDeathDates()
+      return this.parseCsv(data)
+      // const raw = this.parseCsv(data)
+      // this.deathsHdr = raw[0]
+      // this.deaths = raw.slice(1)
+      // this.extractDeathDates()
     },
-    extractCasesDates() {
-      this.casesIdx = this.casesHdr.findIndex((i) => i === 'Combined_Key') + 1
-      this.casesDates = this.casesHdr
-        .slice(this.casesIdx)
-        .map((d) => new Date(d).getTime())
+    extractDates(hdr, datesProp, datesOffset = 1) {
+      const datesIdx = hdr.findIndex((i) => i === datesProp) + datesOffset
+      return {
+        datesIdx,
+        dates: hdr.slice(datesIdx).map((d) => new Date(d).getTime())
+      }
+    },
+    extractStates(hdr, data, statesProp = 'Province_State') {
+      const statesIdx = hdr.findIndex((i) => i === statesProp)
+      return {
+        statesIdx,
+        states: Array.from(new Set(data.map((r) => r[statesIdx]))).sort()
+      }
     },
     extractDeathDates() {
       this.deathsIdx = this.deathsHdr.findIndex((i) => i === 'Population') + 1
@@ -100,32 +144,24 @@ export default {
         .slice(this.deathsIdx)
         .map((d) => new Date(d).getTime())
     },
-    extractStates() {
-      this.statesIdx = this.casesHdr.findIndex((i) => i === 'Province_State')
-
-      this.states = Array.from(
-        new Set(this.cases.map((r) => r[this.statesIdx]))
-      ).sort()
-    },
-    summarize(state) {
+    stateSelected(state) {
+      localStorage.setItem('selectedState', state)
       this.selectedState = state
-      this.summarizeCases(state)
-      this.summarizeDeaths(state)
+      this.summarizeByState()
     },
-    summarizeCases(state) {
-      const byStateCases = this.cases.filter(
-        (r) => r[this.statesIdx] === this.selectedState
+    summarizeByState() {
+      this.summarizeStateCases()
+    },
+    filterBy(arr, prop, colIdx) {
+      return arr.filter((r) => r[colIdx] === prop)
+    },
+    summarizeStateCases() {
+      const filteredCases = this.filterBy(
+        this.casesInfo.cases,
+        this.selectedState,
+        this.casesInfo.statesIdx
       )
-
-      this.casesTitle = `Cases (Daily Increase) -- ${state}`
-      this.totalCases = byStateCases
-        .map((r) => r.slice(this.casesIdx))
-        .reduce((cum, cols) => {
-          if (cum.length === 0) {
-            cum = Array(cols.length).fill(0)
-          }
-          return cols.map((col, idx) => (cum[idx] += parseInt(col)))
-        }, [])
+      this.totalCases = this.calcTotals(filteredCases, this.casesInfo.datesIdx)
     },
     summarizeDeaths(state) {
       const byStateCases = this.deaths.filter(
@@ -157,6 +193,10 @@ export default {
 </script>
 
 <style scoped>
+.index-container {
+  padding: 6px;
+}
+
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -173,6 +213,10 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+}
+
+.region-select {
+  width: 50%;
 }
 
 .links {
