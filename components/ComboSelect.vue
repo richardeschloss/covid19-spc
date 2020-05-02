29a9cccd228e03e2bac9a321c691a79aeedaa3e9
Richard Schloss <richard.e.schloss@gmail.com>
2020-05-02 16:29:51 -0700

    Replaced StateSelect with more generic ComboSelect

---
+++ components/ComboSelect.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+      <input
+        v-model="selectedItem"
+        :placeholder="placeholder"
+        list="items"
+        class="form-control"
+      />
+    </div>
+    <datalist id="items">
+      <option v-for="item in items" :key="item" :value="item" />
+    </datalist>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+    placeholder: {
+      type: String,
+      default: () => ''
+    },
+    value: {
+      type: String,
+      default: () => ''
+    }
+  },
+  computed: {
+    selectedItem: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        if (val && this.items.includes(val)) {
+          this.$emit('itemSelected', val)
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
